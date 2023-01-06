180c0a5d181efe49de436774d864b400f7959422
justinthelaw <justinwingchunglaw@gmail.com>
2023-01-06 09:19:52 -0800

    App component testing complete

--- react/src/components/App.test.tsx
+++ react/src/components/App.test.tsx
@@ -1,6 +1,8 @@
+import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 import { DEFAULT_NAME } from "../constants/constants";
+import userEvent from "@testing-library/user-event";
 
 test("renders App", () => {
   render(<App />);
@@ -9,20 +11,37 @@ test("renders App", () => {
 test("renders name input label", () => {
   render(<App />);
   // gets the input label
-  const inputLabelElement = screen.getByText(/Enter Your Name:/i).innerHTML;
+  const inputLabelElement: string =
+    screen.getByText(/Enter Your Name:/i).innerHTML;
   expect(inputLabelElement).toBeTruthy();
 });
 
 test("renders name input area", () => {
   render(<App />);
   // gets the input box
-  const inputElement = screen.getByRole("textbox");
+  const inputElement: HTMLElement = screen.getByRole("textbox");
   expect(inputElement).toBeTruthy();
 });
 
 test("renders the user welcome message", () => {
   render(<App />);
   // gets the message by text
-  const messageElement = screen.getByText(`Hello, ${DEFAULT_NAME}`).innerHTML;
+  const messageElement: string = screen.getByText(
+    `Hello, ${DEFAULT_NAME}`
+  ).innerHTML;
+  expect(messageElement).toBeTruthy();
+});
+
+test("renders state changes for user welcome message", () => {
+  render(<App />);
+  // mock a new change state
+  const newUserNameState: string = "New User";
+  const input: HTMLElement = screen.getByRole("textbox");
+  userEvent.clear(input);
+  userEvent.type(input, newUserNameState);
+  // gets the message by text
+  const messageElement: string = screen.getByText(
+    `Hello, ${newUserNameState}`
+  ).innerHTML;
   expect(messageElement).toBeTruthy();
 });
