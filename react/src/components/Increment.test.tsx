924030790e3f264e8a14f0b9aa9ff22b87cc8495
justinthelaw <justinwingchunglaw@gmail.com>
2023-01-06 09:28:21 -0800

    Increment tests in-progress

---
+++ react/src/components/Increment.test.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Increment from "./Increment";
+
+test("renders Increment", () => {
+  render(<Increment />);
+});
